286ee799b499bb5087e03798836e8b200d316491
dealmaker <alex.likhvar@gmail.com>
2020-01-22 22:51:32 +0300

    trying switch in engine

--- src/games/calc.js
+++ src/games/calc.js
@@ -4,18 +4,20 @@ import { getRandomNumber, cons } from '../fasade';
 const description = 'What is the result of the expression?';
 const operators = '-+*';
 
-const calculator = (firstMember, operatorIndex, secondMember) => {
-  if (operatorIndex === 0) return firstMember - secondMember;
-  if (operatorIndex === 1) return firstMember + secondMember;
-  return firstMember * secondMember;
-};
-
 const logic = () => {
   const firstMember = getRandomNumber(1, 100);
   const secondMember = getRandomNumber(1, 100);
   const operatorIndex = getRandomNumber(0, 2);
   const eqation = `${firstMember} ${operators.charAt(operatorIndex)} ${secondMember}`;
-  const rightAnswer = `${(calculator(firstMember, operatorIndex, secondMember))}`;
+  let rightAnswer = 0;
+  switch (operators.charAt(operatorIndex)) {
+    case '-': rightAnswer = firstMember - secondMember;
+      break;
+    case '+': rightAnswer = firstMember + secondMember;
+      break;
+    default: rightAnswer = firstMember * secondMember;
+  }
+  
   return cons(eqation, rightAnswer);
 };
 
